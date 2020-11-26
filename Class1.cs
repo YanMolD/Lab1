541fbcfd4b5dc03486eec59027e6de4d009e5e99
Yan Molodtsov <yan@molodtsov.info>
2020-11-26 20:19:02 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Matrix
+    {
+        private int rows, cols;
+        private int[,] mass;
+
+        public int ROWS
+        {
+            get { return rows; }
+            set { if (value > 0) rows = value; }
+        }
+
+        public int COLS
+        {
+            get { return cols; }
+            set { if (value > 0) cols = value; }
+        }
+
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            mass = new int[rows, cols];
+        }
+
+        public void EnterMatrix()
+        {
+            if (rows <= 0 || cols <= 0)
+                throw new ArgumentException();
+            //Random rn = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
+                    mass[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+        /*ЗАМЕНА ЭЛЕМЕНТА В МАТРИЦЕ*/
+
+        public static Matrix Change(Matrix a, int ChangeRows, int ChangeCols, int Changable)
+        {
+            if ((ChangeRows > a.ROWS) || (ChangeCols > a.COLS))
+                throw new IndexOutOfRangeException();
+            else
+            {
+                for (int i = 1; i <= a.ROWS; i++)
+                {
+                    for (int j = 1; j <= a.COLS; j++)
+                    {
+                        if (ChangeRows == i && ChangeCols == j)
+                        {
+                            a.mass[i - 1, j - 1] = Changable;
+                            break;
+                        }
+                    }
+                }
+            }
+            return a;
+        }
+
+        /*УМНОЖЕНИЕ МАТРИЦЫ НА СКАЛЯР*/
+
+        public static Matrix NUM(Matrix a, int num)
+        {
+            Matrix array = new Matrix(a.ROWS, a.COLS);
+            for (int i = 0; i < a.ROWS; i++)
+            {
+                for (int j = 0; j < a.COLS; j++)
+                {
+                    array.mass[i, j] = a.mass[i, j] * num;
+                }
+            }
+            return array;
+        }
+
+        /*ВЫЧИТАНИЕ МАТРИЦ*/
+
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            Matrix array = new Matrix(a.ROWS, a.COLS);
+            if ((a.ROWS != b.ROWS) || (a.COLS != b.COLS))
+                throw new InvalidOperationException();
+            else
+            {
+                for (int i = 0; i < a.ROWS; i++)
+                {
+                    for (int j = 0; j < b.COLS; j++)
+                    {
+                        array.mass[i, j] = a.mass[i, j] - b.mass[i, j];
+                    }
+                }
+            }
+            return array;
+        }
+
+        /*СЛОЖЕНИЕ МАТРИЦ*/
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            Matrix array = new Matrix(a.ROWS, a.COLS);
+
+            if ((a.ROWS != b.ROWS) || (a.COLS != b.COLS))
+                throw new InvalidOperationException();
+            else
+            {
+                for (int i = 0; i < a.ROWS; i++)
+                {
+                    for (int j = 0; j < b.COLS; j++)
+                    {
+                        array.mass[i, j] = a.mass[i, j] + b.mass[i, j];
+                    }
+                }
+            }
+            return array;
+        }
+
+        /*УМНОЖЕНИЕ МАТРИЦ*/
+
+        public static Matrix Multiply(Matrix MatrixA, Matrix MatrixB)
+        {
+            Matrix MatrixC = new Matrix(MatrixA.ROWS, MatrixB.COLS);
+
+            if (MatrixA.COLS != MatrixB.ROWS)
+                throw new InvalidOperationException();
+            else
+            {
+                for (int i = 0; i < MatrixA.ROWS; i++)
+                    for (int j = 0; j < MatrixB.COLS; j++)
+                    {
+                        for (int r = 0; r < MatrixA.COLS; r++)
+                            MatrixC.mass[i, j] += MatrixA.mass[i, r] * MatrixB.mass[r, j];
+                    }
+            }
+            return MatrixC;
+        }
+
+        public static int Determinant(Matrix a)
+        {
+            int rows = a.COLS, determ = 0, minor_determ, parity;
+
+            if (a.ROWS != a.COLS)
+                throw new InvalidOperationException();
+            else
+            {
+                if (rows == 2)
+                    return a.mass[0, 0] * a.mass[1, 1] - a.mass[0, 1] * a.mass[1, 0];
+                for (int i = 0; i < rows; i++)
+                {
+                    minor_determ = Determinant(Create_minor(a, i));
+                    parity = ((i & 1) == 0 ? 1 : -1);
+                    determ += parity * a.mass[0, i] * minor_determ;
+                }
+            }
+            return determ;
+        }
+
+        public static Matrix Create_minor(Matrix a, int coloumn)
+        {
+            int rows = a.COLS - 1;
+            int count_coloumn = 0;
+            Matrix Minor = new Matrix(rows, rows);
+            for (int i = 1; i < rows + 1; i++)
+            {
+                for (int j = 0; j < rows + 1; j++)
+                    if (j != coloumn)
+                        Minor.mass[i - 1, count_coloumn++] = a.mass[i, j];
+                count_coloumn = 0;
+            }
+            return Minor;
+        }
+
+        ~Matrix()
+        {
+            Console.WriteLine("Очистка");
+        }
+
+        public override string ToString()
+        {
+            string buf = "";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    buf += (mass[i, j] + "\t");
+                }
+                buf += "\n";
+            }
+            return buf;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class MainProgram
+    {
+        private static void Main(string[] args)
+        {
+            //СКАЛЯР
+            int num = 10;
+            //ПАРАМЕТРЫ ДВУХ МАТРИЦ
+            int rows = 2, cols = 2;
+            int rows2 = 3, cols2 = 2;
+            //ПАРАМЕТРЫ ЗАМЕНЯЕМОГО ЭЛЕМЕНТА
+            int ChangeRows = 1, ChangeCols = 1, Changable = 10;
+
+            Matrix A = new Matrix(rows, cols);
+            Matrix B = new Matrix(rows2, cols2);
+            Matrix Res = new Matrix(rows, cols2);
+
+        Enter:
+            try
+            {
+                Console.WriteLine("ввод Матрица А: ");
+                A.EnterMatrix();
+                Console.WriteLine("Ввод Матрица B: ");
+                B.EnterMatrix();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Матрица не существует: {ex.Message}");
+                Environment.Exit(0);
+            }
+            catch
+            {
+                Console.WriteLine("Недопустимое значение элемента");
+                goto Enter;
+            }
+
+            Console.WriteLine("Матрица А: ");
+            Console.Write(A);
+            Console.WriteLine();
+            Console.WriteLine("Матрица В: ");
+            Console.Write(B);
+            Console.WriteLine();
+
+            Console.WriteLine("Замена элемента:");
+            try
+            {
+                Matrix.Change(A, ChangeRows, ChangeCols, Changable);
+                Console.Write(A);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Такого элемента не существует: {ex.Message}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Данный формат не inetger");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Вычитание:");
+            try
+            {
+                Res = (A - B);
+                Console.Write(Res);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Вычитание недопустимо: {ex.Message}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сложение:");
+            try
+            {
+                Res = (A + B);
+                Console.Write(Res);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Сложение недопустимо: {ex.Message}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Умножение на скаляр:");
+            Res = Matrix.NUM(A, num);
+            Console.Write(Res);
+            Console.WriteLine();
+
+            Console.WriteLine("Умножение матриц:");
+            try
+            {
+                Console.Write(Matrix.Multiply(A, B));
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Умножение недопустимо: {ex.Message}");
+            }
+
+            try
+            {
+                Console.WriteLine("Детерминант матрицы А равен {0}\nДетерминант матрицы B равен {1}", Matrix.Determinant(A), Matrix.Determinant(B));
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Невозможно посчитать определитель: {ex.Message}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
